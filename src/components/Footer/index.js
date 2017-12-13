5a604e4f6a551e635341509b4ee66cf1e4e73a64
Thomas Demkey <thomasd@criticalmass.com>
2017-12-13 09:28:20 -0700

    Work on Footer component.

---
+++ src/components/Footer/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Animated } from 'react-animated-css';
+import Logo_Extra_Life from '../../assets/logo_extra_life.svg';
+
+const StyledFooter = styled.div`
+
+`;
+
+class Footer extends Component {
+    static propTypes = {
+
+    }
+
+    static defaultProps = {
+
+    }
+    render() {
+        return (
+            <StyledFooter>
+                
+            </StyledFooter>
+        );
+    }
+
+}
+
+export default Footer;
